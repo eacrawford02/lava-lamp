052a72ca0b79e956e2c1df664304659a6c3733a8
Ewen Crawford <eacrawford02@gmail.com>
2022-09-12 21:16:45 -0400

    Corrected buffer design to use BRAM resources, reorganized buffer module testbench

--- sim/tb-buffer.sv
+++ sim/tb-buffer.sv
@@ -9,19 +9,16 @@ module tb_buffer #(
     input event done
 );
   logic clk     = 0,
-	rst     = 0,
 	en      = 0,
 	swap_en = 0,
 	w_en    = 0;
   logic [9:0]  addr;
-  logic [11:0] din,
-	       dout;
+  logic [11:0] din, dout;
 
   always #10 clk <= ~clk;
 
   buffer dut(
     .clk(clk),
-    .rst(rst),
     .en(en),
     .swap_en(swap_en),
     .w_en(w_en),
@@ -30,33 +27,42 @@ module tb_buffer #(
     .dout(dout)
   );
 
+  function automatic void mem_status(string op);
+    logic [11:0] a_out = dut.buff_a.bram[addr];
+    logic [11:0] b_out = dut.buff_b.bram[addr];
+    logic ptr = dut.cur_buff;
+    $display("%s\t%h\t%h\t%h\t%h\t%b", op, addr, a_out, b_out, dout, ptr);
+  endfunction
+
   logic [9:0] gen_addr;
   logic [11:0] gen_data;
-  string status;
+  string buff, status;
+  logic [9:0] word_a, word_b;
 
   initial begin
     wait (start.triggered);
     $display("=== BUFFER TEST STARTED ===");
-    $display("Address\tExpected\tActual\tStatus");
-    // Drive test signals in sync with clock
-    @ (posedge clk);
-    // Write data then signal buffer swap
+    $display("Test\tAddr\tBuff A\tBuff B\tOutput\tBuff Ptr");
+    // Assert test data
+    gen_addr = $urandom_range(0, RAM_DEPTH-1);
+    gen_data = $urandom_range(0, DATA_RANGE-1);
+    addr = gen_addr;
+    din = gen_data;
+    // Test write
     en = 1;
     w_en = 1;
-    gen_addr = $urandom_range(0, RAM_DEPTH-1);
-    addr = gen_addr;
-    gen_data = $urandom_range(0, DATA_RANGE-1);
-    din = gen_data;
-    swap_en = 1;
-    // Deassert signals and check output after buffer swap
     @ (posedge clk);
-    en = 0;
+    mem_status("Write");
+    // Test Swap
     w_en = 0;
+    swap_en = 1;
+    @ (posedge clk);
+    mem_status("Swap");
+    // Check output after buffer swap
     swap_en = 0;
-    if (dout == gen_data) status = "PASS";
-    else status = "FAIL";
-    $display("%h\t%h\t\t%h\t%s", addr, gen_data, dout, status);
-    $display("=== BUFFER TEST FINISHED ===");
+    @ (posedge clk);
+    mem_status("Hold En");
+   $display("=== BUFFER TEST FINISHED ===");
     ->done;
   end
 endmodule
--- src/buffer.sv
+++ src/buffer.sv
@@ -2,7 +2,6 @@
 `timescale 1ns / 1ps
 module buffer (
     input		clk,
-    input		rst,
     input		en,
     input		swap_en,
     input		w_en,
@@ -10,27 +9,57 @@ module buffer (
     input  logic [11:0] din,
     output logic [11:0] dout
 );
-  logic [11:0] buff_a [1023:0];
-  logic [11:0] buff_b [1023:0];
-  logic cur_buff = 0;
+
+  logic [11:0] a_out,
+               b_out;
+  logic        w_en_a,
+               w_en_b;
+  logic        cur_buff = 0;
+
+  assign w_en_a = w_en & ~cur_buff;
+  assign w_en_b = w_en & cur_buff;
+
+  bram_sp_rf buff_a (
+    .clk(clk),
+    .en(en),
+    .w_en(w_en_a),
+    .addr(addr),
+    .din(din),
+    .dout(a_out)
+  );
+
+  bram_sp_rf buff_b (
+    .clk(clk),
+    .en(en),
+    .w_en(w_en_b),
+    .addr(addr),
+    .din(din),
+    .dout(b_out)
+  );
+  
+
+  always_ff @ (posedge clk) begin
+      if (en & swap_en) cur_buff <= ~cur_buff;
+  end
+
+  // Opposite of what the current buffer should be
+  assign dout = cur_buff ? a_out : b_out;
+endmodule
+
+module bram_sp_rf (
+  input		      clk,
+  input		      en,
+  input		      w_en,
+  input  logic [9:0]  addr,
+  input  logic [11:0] din,
+  output logic [11:0] dout
+);
+  logic [11:0] bram [1023:0];
 
   always_ff @ (posedge clk) begin
     if (en) begin
-      if (swap_en) begin
-	cur_buff <= ~cur_buff;
-      end
-      // Write to current buffer, swap_en must settle before writes reflect
-      // buffer swap (i.e., must wait until next clock edge to write to new
-      // buffer)
-      if (w_en) begin
-	case (cur_buff)
-	  0: buff_a[addr] <= din;
-	  1: buff_b[addr] <= din;
-	endcase
-	dout <= din; // Write first
-      end
-    end else begin
-      dout <= ~cur_buff ? buff_a[addr] : buff_b[addr];
+      if (w_en) bram[addr] <= din;
+      dout <= bram[addr]; // Read-first
     end
   end
 endmodule
