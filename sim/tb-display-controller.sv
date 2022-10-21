5ab2f2bfe81a658e86e6402bc468b9d305120d10
Ewen Crawford <eacrawford02@gmail.com>
2022-10-21 17:22:42 -0400

    Added data input to dut

--- sim/tb-display-controller.sv
+++ sim/tb-display-controller.sv
@@ -7,7 +7,7 @@ module tb_dspl_ctrl (
 );
   logic clk = 0,
 	rst = 0;
-  logic [11:0] din_top, din_btm;
+  logic [11:0] din_top = 0, din_btm = 0;
   logic [9:0] r_addr;
   logic sclk, latch, blank;
   logic [2:0] dout_top, dout_btm;
@@ -29,11 +29,20 @@ module tb_dspl_ctrl (
     .row_sel(row_sel)
   );
 
+  logic [9:0] prev_r_addr = 0;
+  always @ (posedge clk) begin
+    if (prev_r_addr != r_addr) begin
+      din_top <= din_top + 1;
+      din_btm <= din_btm + 1;
+    end
+    prev_r_addr <= r_addr;
+  end
+
   initial begin
     wait (start.triggered);
     $display("=== DISPLAY CONTROLLER TEST STARTED ===");
-    $display("=== DISPLAY CONTROLLER TEST FINISHED ===");
     #38400;
+    $display("=== DISPLAY CONTROLLER TEST FINISHED ===");
     ->done;
   end
 endmodule
