aaff2a5fca801544b156c338c3f08d5fba88ba4c
Ewen Crawford <eacrawford02@gmail.com>
2023-01-06 15:30:45 -0500

    Added logic to prevent metaball from getting stuck out of bounds, added min and max compiler directives

--- src/metaball.sv
+++ src/metaball.sv
@@ -1,12 +1,20 @@
 // Copyright (C) 2022  Ewen Crawford
 `timescale 1ns / 1ps
 
+`ifndef MAX
+`define MAX(a, b) ((a) > (b) ? (a) : (b))
+`endif
+
+`ifndef MIN
+`define MIN(a, b) ((a) > (b) ? (b) : (a))
+`endif
+
 module metaball #(
     parameter WIDTH  = 32'h000f_8000, // Display width - 1 (x-axis) = 31
-    parameter HEIGHT = 32'h000f_c000, // Display height - 1 (y-axis) = 63
+    parameter HEIGHT = 32'h001f_8000, // Display height - 1 (y-axis) = 63
     parameter RAD    = 32'h0001_4000, // Initial radius = 2.5
     parameter I_X    = 32'h0007_c000, // Initial x-position = 15.5 (centered)
-    parameter I_Y    = 32'h0000_0000, // Initial y-position = 0 (bottom)
+    parameter I_Y    = 32'h000f_8000, // Initial y-position = 31 (centered)
     // Speed hardcoded for 60 Hz update rate
     parameter IV_X   = 32'h0000_0000, // Initial x-speed = 0 px/cycle (0 px/s)
     parameter IV_Y   = 32'h0000_0333  // Initial y-speed = 0.025 px/cycle 
@@ -25,8 +33,10 @@ module metaball #(
   );
 
   // Position
-  logic [31:0] x = I_X,
-	       y = I_Y;
+  logic [31:0] x      = `MAX(RAD + 32'h1, `MIN(I_X, WIDTH - RAD - 32'h1)),
+	       y      = `MAX(RAD + 32'h1, `MIN(I_Y, HEIGHT - RAD - 32'h1)),
+	       next_x = I_X + IV_X,
+	       next_y = I_Y + IV_Y;
   // Speed
   logic [31:0] v_x = IV_X,
 	       v_y = IV_Y;
@@ -34,12 +44,15 @@ module metaball #(
   // For all addition/subtraction operations, we assume the numbers are
   // unsigned, i.e., the lower bounds of the coordinate system is 0 (no
   // negative positions)
-  wire [31:0] dx, dy, dx_sq, dy_sq;
+  wire [31:0] dx, dy, dx_abs, dy_abs, dx_sq, dy_sq;
   // Because the qmult module doesn't use the sign bits of the multiplicand or
   // multiplier (dx[31] and dy[31] in this case), Vivado may throw a warning
   // that p_x[31] and p_y[31] are unconnected. This is to be expected
   assign dx = p_x - x;
   assign dy = p_y - y;
+  // The qmult module can't do signed multiplication correctly
+  assign dx_abs = dx[31] ? ~dx + 1 : dx;
+  assign dy_abs = dy[31] ? ~dy + 1 : dy;
 
   wire [63:0] rad_sq;
   wire [31:0] dividend, divisor;
@@ -47,14 +60,14 @@ module metaball #(
   assign dividend = rad_sq[45:15];
 
   qmult #(15,32) sq1(
-    .i_multiplicand(dx),
-    .i_multiplier(dx),
+    .i_multiplicand(dx_abs),
+    .i_multiplier(dx_abs),
     .o_result(dx_sq),
     .ovr()
   );
   qmult #(15,32) sq2(
-    .i_multiplicand(dy),
-    .i_multiplier(dy),
+    .i_multiplicand(dy_abs),
+    .i_multiplier(dy_abs),
     .o_result(dy_sq),
     .ovr()
   );
@@ -73,13 +86,26 @@ module metaball #(
     if (rst) begin
       x <= I_X;
       y <= I_Y;
+      next_x <= I_X + IV_X;
+      next_y <= I_Y + IV_Y;
     end else begin
       if (mov_en) begin
-	x <= x + v_x;
-	y <= y + v_y;
-	// Negate speed vectors when bounds of display are reached
-	if (x >= (WIDTH - RAD) || x <= RAD) v_x = ~v_x + 1;
-	if (y >= (HEIGHT - RAD) || y <= RAD) v_y = ~v_y + 1;
+	x <= next_x;
+	y <= next_y;
+	// Negate speed vectors when bounds of display are reached and prevent
+	// metaball from getting stuck outside bounds
+	if (next_x >= (WIDTH - RAD) || next_x <= RAD) begin
+	  v_x <= ~v_x + 1;
+	  next_x <= x;
+	end else begin
+	  next_x <= x + v_x;
+	end
+	if (next_y >= (HEIGHT - RAD) || next_y <= RAD) begin
+	  v_y <= ~v_y + 1;
+	  next_y <= y;
+	end else begin
+	  next_y <= y + v_y;
+	end
       end
     end
   end
