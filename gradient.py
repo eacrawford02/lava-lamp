02effc37c72c44da5ad487e7ac90794a7769e59a
Ewen Crawford <eacrawford02@gmail.com>
2023-03-09 23:36:29 -0500

    Corrected colour data memory file layout

--- gradient.py
+++ gradient.py
@@ -1,3 +1,5 @@
+import itertools
+
 topColour = [0, 0, 15]
 bottomColour = [15, 0, 0]
 
@@ -6,7 +8,7 @@ height = 64
 
 f = open("colour.data", 'w')
 
-rowColours = []
+rowColours = [[0 for x in range(width)] for y in range(height)]
 
 for i in range(height):
     deltaY = [t - b for (t, b) in zip(topColour, bottomColour)]
@@ -14,8 +16,14 @@ for i in range(height):
     colour = [round(m * i + b) for (m, b) in zip(slope, bottomColour)]
     unicode = [(ord('0') + c if c < 10 else ord('A') + c - 10) for c in colour]
     concat = ''.join([chr(char) for char in unicode])
-    rowColours += [concat + '\n'] * width # Duplicate for each column in row
+    rowColours[i] = [concat + '\n'] * width # Duplicate for each column in row
 
-f.writelines(rowColours[0:-1]) # Drop the trailing newline
+# Transpose colour matrix so that colour values along the gradient axis are 
+# written to the file in sequence (i.e., column by column). This corresponds 
+# with the order that pixel values are read from the ROM in hardware
+memFormat = list(zip(*rowColours))
+flatData = list(itertools.chain(*memFormat)) # Unzip matrix and convert to list
+
+f.writelines(flatData)
 
 f.close()
